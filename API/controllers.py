803500db7ed0f937a13edde892fb32a17952a6e5
JOSE <joseantonioramosandrades@gmail.com>
2023-05-18 18:59:13 +0200

    Added success message

--- API/controllers.py
+++ API/controllers.py
@@ -35,6 +35,8 @@ def pybytes_integration():
     data = request.get_json()
     print(data)
 
+    return "Success", 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=80)
