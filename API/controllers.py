14e2550883087df99aeec5e935adb9059e4cae55
JOSE <joseantonioramosandrades@gmail.com>
2023-05-19 18:16:31 +0200

    Added status handling on API

--- API/controllers.py
+++ API/controllers.py
@@ -14,9 +14,10 @@ def monitor_data():
         for service in services.keys():
             print(service)
             name = services[service]["name"]
-            for key in ["cpu_percent", "memory_rss"]:
+
+            for key in ["status", "cpu_percent", "memory_rss"]:
                 data_name = f"{name}.{key}"
-                type = DatabaseContext.DataType.FLOAT
+                type = DatabaseContext.DataType.BOOL if key == "status" else DatabaseContext.DataType.FLOAT
                 DatabaseContext().store_value(data_name, services[service][key], type, device_id)
 
         for key in ["load_avg", "mem"]:
